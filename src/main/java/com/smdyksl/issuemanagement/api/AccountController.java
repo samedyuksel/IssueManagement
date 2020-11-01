da5b8d131f12aef6791bd2edaa129cbc57311f87
samedyuksel <yukselsamed@hotmail.com>
2020-11-01 13:46:44 +0300

    security added backend

--- src/main/java/com/smdyksl/issuemanagement/api/AccountController.java
+++ src/main/java/com/smdyksl/issuemanagement/api/AccountController.java
@@ -1,5 +1,46 @@
 package com.smdyksl.issuemanagement.api;
 
+import com.smdyksl.issuemanagement.dto.LoginRequest;
+import com.smdyksl.issuemanagement.dto.RegistrationRequest;
+import com.smdyksl.issuemanagement.dto.TokenResponse;
+import com.smdyksl.issuemanagement.entity.User;
+import com.smdyksl.issuemanagement.repository.UserRepository;
+import com.smdyksl.issuemanagement.security.JwtTokenUtil;
+import com.smdyksl.issuemanagement.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/token")
 public class AccountController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserServiceImpl userService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) throws AuthenticationException {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        final User user = userRepository.findByUsername(request.getUsername());
+        final String token = jwtTokenUtil.generateToken(user);
+        return ResponseEntity.ok(new TokenResponse(user.getUsername(), token));
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<Boolean> register(@RequestBody RegistrationRequest registrationRequest) throws AuthenticationException {
+        Boolean response = userService.register(registrationRequest);
+        return ResponseEntity.ok(response);
+    }
 
 }
